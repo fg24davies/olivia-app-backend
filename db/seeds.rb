5549d8b6d8d4fb68b68a370f7c248ebf173d555b
Flora Davies <daviesfgo@gmail.com>
2021-09-19 21:36:32 +0100

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.new(username: "user1", password: "123456").save
