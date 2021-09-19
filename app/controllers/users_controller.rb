bf689bcc7dfdc33d598f5e399572b492a18c8aa2
Flora Davies <daviesfgo@gmail.com>
2021-09-19 20:12:42 +0100

    Uppload to git

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
